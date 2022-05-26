b0166c0bd33f57e6c1961200eca348cecb7abbbe
JairoJS1 <jairo.j199912@gmail.com>
2022-05-26 01:55:02 -0600

    configuración para heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/examen-final-front'));
+app.use(express.static('./dist/aeropuerto-frontend'));
 
 app.get('/*', function(req,res) {
 
